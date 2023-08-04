9cac11ed1115b8c862200af214b16e51bed4c38d
CuongLuu5 <l.q.c1985@gmail.com>
2023-08-04 23:32:10 +0700

    update assetcontroller
    assetcontroller

--- src/DCOneCrypto.Interface/DCOneCrypto.Api/Controllers/AssetController.cs
+++ src/DCOneCrypto.Interface/DCOneCrypto.Api/Controllers/AssetController.cs
@@ -36,7 +36,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_addresses")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetAddresses(string _asset_policy, string _asset_name)
+        public async Task<ActionResult<string>> GetAssetAddresses(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name = "52455655")
         {
             var res = await _assetService.GetAssetAddresses(_asset_policy, _asset_name);
             return Ok(res);
@@ -44,7 +44,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_nft_address")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetNFTAddress(string _asset_policy, string _asset_name)
+        public async Task<ActionResult<string>> GetAssetNFTAddress(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name= "52455655")
         {
             var res = await _assetService.GetAssetNFTAddress(_asset_policy, _asset_name);
             return Ok(res);
@@ -52,7 +52,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_info")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetInfo(string _asset_policy, string _asset_name)
+        public async Task<ActionResult<string>> GetAssetInfo(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name= "52455655")
         {
             var res = await _assetService.GetAssetInfo(_asset_policy, _asset_name);
             return Ok(res);
@@ -60,7 +60,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_history")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetHistory(string _asset_policy, string _asset_name)
+        public async Task<ActionResult<string>> GetAssetHistory(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name= "52455655")
         {
             var res = await _assetService.GetAssetHistory(_asset_policy, _asset_name);
             return Ok(res);
@@ -68,7 +68,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("policy_asset_addresses")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetPolicyAssetAddresses(string _asset_policy)
+        public async Task<ActionResult<string>> GetPolicyAssetAddresses(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864")
         {
             var res = await _assetService.GetPolicyAssetAddresses(_asset_policy);
             return Ok(res);
@@ -76,7 +76,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("policy_asset_info")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetPolicyAssetInfo(string _asset_policy)
+        public async Task<ActionResult<string>> GetPolicyAssetInfo(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864")
         {
             var res = await _assetService.GetPolicyAssetInfo(_asset_policy);
             return Ok(res);
@@ -85,7 +85,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("policy_asset_list")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetPolicyAssetList(string _asset_policy)
+        public async Task<ActionResult<string>> GetPolicyAssetList(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864")
         {
             var res = await _assetService.GetPolicyAssetList(_asset_policy);
             return Ok(res);
@@ -93,7 +93,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_summary")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetSumary(string _asset_policy, string _asset_name)
+        public async Task<ActionResult<string>> GetAssetSumary(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name = "52455655"    )
         {
             var res = await _assetService.GetAssetSumary(_asset_policy, _asset_name);
             return Ok(res);
@@ -101,7 +101,7 @@ namespace DCOneCrypto.Api.Controllers
 
         [Route("asset_txs")]
         [HttpGet]
-        public async Task<ActionResult<string>> GetAssetTransactions(string _asset_policy, string _asset_name, int _after_block_height, bool _history)
+        public async Task<ActionResult<string>> GetAssetTransactions(string _asset_policy= "94cbb4fcbcaa2975779f273b263eb3b5f24a9951e446d6dc4c135864", string _asset_name= "52455655", int _after_block_height= 8547193, bool _history=false)
         {
             var res = await _assetService.GetAssetTransactions(_asset_policy, _asset_name, _after_block_height, _history);
             return Ok(res);
